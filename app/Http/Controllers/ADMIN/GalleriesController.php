1f1e163ab4d4d57f72e9d63766e4e633078811ef
RAMANANA Thu Ming Thierry <ramananathumingthierry@gmail.com>
2025-04-13 17:32:38 +0200

    Correction

--- app/Http/Controllers/ADMIN/GalleriesController.php
+++ app/Http/Controllers/ADMIN/GalleriesController.php
@@ -28,18 +28,18 @@ class GalleriesController extends Controller
 
             if ($request->ajax()) {
                 $galleries = $this->gelleryService->getAllGalleries(['id','image_url','status']);
-    
+
                 return DataTables::of($galleries)
                     ->addColumn('image_url', function ($row) {
                         return '<img src="' . asset($row->image_url) . '" alt="Image" width="100">';
                     })
                     ->addColumn('action', function ($row) {
-                        return '<a href="' . route('admin.galleries.edit', $row->id) . '" class="btn btn-sm btn-primary">Modifier</a>';
+                        return '<a href="' . route('admin.gallery.edit', $row->id) . '" class="btn btn-sm btn-primary">Modifier</a>';
                     })
                     ->rawColumns(['image_url', 'action'])
                     ->make(true);
             }
-    
+
             return view('backoffice.galleries.index');
 
         }catch(Exception $e){
